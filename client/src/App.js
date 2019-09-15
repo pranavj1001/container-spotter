ed4821cdadc01857e5358a9fad9be1c62b7e0bd8
pranavj1001 <pranavj1001@gmail.com>
2019-09-15 17:10:37 +0530

    setup client and nginx with docker

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
