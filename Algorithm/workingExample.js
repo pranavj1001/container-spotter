e00cff652aba9975b5b644eaacf3436ef8e4ec7a
pranavj1001 <pranavj1001@gmail.com>
2019-11-04 00:23:29 +0530

    add raw workingExample
    the code is still wip

--- Algorithm/workingExample.js
+++ Algorithm/workingExample.js
@@ -46,10 +46,13 @@ const data = [{
 // prepare sorted (desc) listOfItems from data
 const listOfItems = [];
 let totalItemsVolume = 0;
-for (const item of data) {
+for (let item of data) {
+    item = JSON.parse(JSON.stringify(item));
     item.volume = item.length * item.breadth * item.height;
     totalItemsVolume += item.volume * item.quantity;
     for (let i = 0; i < item.quantity; i++) {
+        item.id = item.name + i.toString();
+
         listOfItems.push(item);
     }
 }
@@ -61,4 +64,65 @@ const pivots = [{
     xCoordinate: 0,
     yCoordinate: 0,
     zCoordinate: 0
-}]
+}];
+maxPivotXCoordinate = 0;
+maxPivotYCoordinate = 0;
+maxPivotZCoordinate = 0;
+
+// list of packed and not packed items
+packedItems = [];
+notPackedItems = [];
+
+const doesItemFit = (pivot, item) => {
+    // TODO P3: perform item rotations
+    // TODO P2: add three condition to check for max possible length available
+    if (item.length + pivot.xCoordinate > bin.length ||
+        item.breadth + pivot.yCoordinate > bin.breadth ||
+        item.height + pivot.zCoordinate > bin.height) {
+            return false;
+        } else {
+            return true;   
+        }
+};
+
+for (let item of listOfItems) {
+    item = JSON.parse(JSON.stringify(item));
+    itemNotPacked = true;
+    counter = 0;
+    for (const pivot of pivots) {
+        debugger;
+        if (doesItemFit(pivot, item)) {
+            item.pivotPosition = pivot;
+            packedItems.push(item);
+            itemNotPacked = false;
+            maxPivotXCoordinate = 0;
+            maxPivotYCoordinate = 0;
+            maxPivotZCoordinate = 0;
+            pivots.push({
+                xCoordinate: pivot.xCoordinate,
+                yCoordinate: pivot.yCoordinate,
+                zCoordinate: pivot.zCoordinate + item.height
+            });
+            pivots.push({
+                xCoordinate: pivot.xCoordinate,
+                yCoordinate: pivot.yCoordinate + item.breadth,
+                zCoordinate: pivot.zCoordinate
+            });
+            pivots.push({
+                xCoordinate: pivot.xCoordinate + item.length,
+                yCoordinate: pivot.yCoordinate,
+                zCoordinate: pivot.zCoordinate
+            });
+            pivots.splice(counter, 1);
+            // TODO P1: remove the smaller pivot out of two which have same any two co-ordinates
+            break;
+        }
+        counter++;
+    }
+
+    if (itemNotPacked) {
+        notPackedItems.push(item);
+    }
+}
+
+console.log(packedItems, notPackedItems);
