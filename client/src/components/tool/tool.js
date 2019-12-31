4b7bd24aab43f232d1cc67e310e04631d2133beb
Pranav Jain <pranavj1001@gmail.com>
2019-12-31 17:33:23 +0530

    use bootstrap to divide page for controller and visualizer

--- client/src/components/tool/tool.js
+++ client/src/components/tool/tool.js
@@ -4,9 +4,13 @@ import Visualizer from '../visualizer/visualizer';
 
 const Tool = () => {
     return (
-        <div>
-            <Controller />
-            <Visualizer />
+        <div className="row">
+            <div id="controller" className="col-md-3 container-spotter-no-padding-div container container-spotter-left-div">
+                <Controller />
+            </div>
+            <div id="view" className="col-md-9 container-spotter-no-padding-div container-spotter-right-div">
+                <Visualizer />
+            </div>
         </div>
     );
 };
