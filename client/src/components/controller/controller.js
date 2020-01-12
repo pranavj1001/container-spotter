9a2aafd24a0de8d6663783c3de6121e807b20d00
pranavj1001 <pranavj1001@gmail.com>
2020-01-12 22:20:56 +0530

    add bin dimension details in controller component

--- client/src/components/controller/controller.js
+++ client/src/components/controller/controller.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 const Controller = () => {
     return (
-        <div>Controller Component</div>
+        <div className="container-spotter-content-div">
+            <h3>Container Spotter</h3>
+            <hr className="container-spotter-basic-hr" />
+            <h4>Bin Dimensions</h4>
+            <hr className="container-spotter-basic-hr" />
+            <div className="form-group">
+                <div className="row">
+                    <div className="col-lg-4 container-spotter-input-label-block">
+                        <label for="binXCoordinate">Length</label>
+                        <input type="number" className="form-control" name="binXCoordinate" id="binXCoordinate" placeholder="20" />
+                    </div>
+                    <div className="col-lg-4 container-spotter-input-label-block">
+                        <label for="binZCoordinate">Width</label>
+                        <input type="number" className="form-control" name="binZCoordinate" id="binZCoordinate" placeholder="10" />
+                    </div>
+                    <div className="col-lg-4 container-spotter-input-label-block">
+                        <label for="binYCoordinate">Height</label>
+                        <input type="number" className="form-control" name="binYCoordinate" id="binYCoordinate" placeholder="10" />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
