49d643d4b3bcddb980375c58f7dead82f58c45ee
pranavj1001 <pranavj1001@gmail.com>
2020-01-18 23:44:30 +0530

    add bin component files in bin.js

---
+++ client/src/components/bin/bin.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Controller = () => {
+    return (
+        <div>
+            <h4>Bin Dimensions</h4>
+            <hr className="container-spotter-basic-hr" />
+            <div className="form-group">
+                <div className="row">
+                    <div className="col-lg-4 container-spotter-input-label-block">
+                        <label for="binXCoordinate">Length</label>
+                        <input type="number" className="form-control" name="binXCoordinate" id="binXCoordinate" placeholder="20" />
+                    </div>
+                    <div className="col-lg-4 container-spotter-input-label-block">
+                        <label for="binZCoordinate">Width</label>
+                        <input type="number" className="form-control" name="binZCoordinate" id="binZCoordinate" placeholder="10" />
+                    </div>
+                    <div className="col-lg-4 container-spotter-input-label-block">
+                        <label for="binYCoordinate">Height</label>
+                        <input type="number" className="form-control" name="binYCoordinate" id="binYCoordinate" placeholder="10" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Controller;
--- client/src/components/controller/controller.js
+++ client/src/components/controller/controller.js
@@ -1,28 +1,14 @@
 import React from 'react';
+import Bin from '../bin/bin';
 
 const Controller = () => {
     return (
         <div className="container-spotter-content-div">
             <h3>Container Spotter</h3>
             <hr className="container-spotter-basic-hr" />
-            <h4>Bin Dimensions</h4>
-            <hr className="container-spotter-basic-hr" />
-            <div className="form-group">
-                <div className="row">
-                    <div className="col-lg-4 container-spotter-input-label-block">
-                        <label for="binXCoordinate">Length</label>
-                        <input type="number" className="form-control" name="binXCoordinate" id="binXCoordinate" placeholder="20" />
-                    </div>
-                    <div className="col-lg-4 container-spotter-input-label-block">
-                        <label for="binZCoordinate">Width</label>
-                        <input type="number" className="form-control" name="binZCoordinate" id="binZCoordinate" placeholder="10" />
-                    </div>
-                    <div className="col-lg-4 container-spotter-input-label-block">
-                        <label for="binYCoordinate">Height</label>
-                        <input type="number" className="form-control" name="binYCoordinate" id="binYCoordinate" placeholder="10" />
-                    </div>
-                </div>
-            </div>
+            <Bin />
+            <hr className="container-spotter-item-divider-hr" />
+			<button className="btn btn-primary">Pack</button>
         </div>
     );
 };
