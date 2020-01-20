986476a30493d9ba97a589c864fc3d15b3e7883d
pranavj1001 <pranavj1001@gmail.com>
2020-01-20 07:31:15 +0530

    convert Bin to a Class Component from Function
    also add code to set states for change in bin dimensions

--- client/src/components/bin/bin.js
+++ client/src/components/bin/bin.js
@@ -1,28 +1,69 @@
 import React from 'react';
 
-const Controller = () => {
-    return (
-        <React.Fragment>
-            <h4>Bin Dimensions</h4>
-            <hr className="container-spotter-basic-hr" />
-            <div className="form-group">
-                <div className="row">
-                    <div className="col-lg-4 container-spotter-input-label-block">
-                        <label for="binXCoordinate">Length</label>
-                        <input type="number" className="form-control" name="binXCoordinate" id="binXCoordinate" placeholder="20" />
-                    </div>
-                    <div className="col-lg-4 container-spotter-input-label-block">
-                        <label for="binZCoordinate">Width</label>
-                        <input type="number" className="form-control" name="binZCoordinate" id="binZCoordinate" placeholder="10" />
-                    </div>
-                    <div className="col-lg-4 container-spotter-input-label-block">
-                        <label for="binYCoordinate">Height</label>
-                        <input type="number" className="form-control" name="binYCoordinate" id="binYCoordinate" placeholder="10" />
+class Bin extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            binXCoordinate: 0,
+            binZCoordinate: 0,
+            binYCoordinate: 0
+        };
+    }
+
+    handleChange = ({ target }) => {
+        this.setState({ [target.name]: target.value });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <h4>Bin Dimensions</h4>
+                <hr className="container-spotter-basic-hr" />
+                <div className="form-group">
+                    <div className="row">
+                        <div className="col-lg-4 container-spotter-input-label-block">
+                            <label htmlFor="binXCoordinate">Length</label>
+                            <input 
+                                type="number" 
+                                className="form-control" 
+                                name="binXCoordinate" 
+                                id="binXCoordinate" 
+                                placeholder="20" 
+                                value={this.state.binXCoordinate}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="col-lg-4 container-spotter-input-label-block">
+                            <label htmlFor="binZCoordinate">Width</label>
+                            <input 
+                                type="number" 
+                                className="form-control" 
+                                name="binZCoordinate" 
+                                id="binZCoordinate" 
+                                placeholder="10" 
+                                value={this.state.binZCoordinate}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="col-lg-4 container-spotter-input-label-block">
+                            <label htmlFor="binYCoordinate">Height</label>
+                            <input 
+                                type="number" 
+                                className="form-control" 
+                                name="binYCoordinate" 
+                                id="binYCoordinate" 
+                                placeholder="10" 
+                                value={this.state.binYCoordinate}
+                                onChange={this.handleChange}
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </React.Fragment>
-    );
-};
+            </React.Fragment>
+        );
+    }
 
-export default Controller;
+}
+
+export default Bin;
