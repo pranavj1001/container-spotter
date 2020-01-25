0d2aaac88584cd2ff974c1c7270252df81e3e4b0
pranavj1001 <pranavj1001@gmail.com>
2020-01-25 20:59:30 +0530

    udpate local state of item component

--- client/src/components/item/item.js
+++ client/src/components/item/item.js
@@ -10,32 +10,89 @@ class Item extends React.Component {
         };
     }
 
+    handleChange = ({ target }) => {
+        this.setState((previousState) => { 
+            return {
+                index: previousState.index,
+                itemDetails: {
+                    itemLength: target.name !== 'itemLength' ? previousState.itemDetails.itemLength : parseFloat(target.value) || '',
+                    itemWidth: target.name !== 'itemWidth' ? previousState.itemDetails.itemWidth : parseFloat(target.value) || '',
+                    itemHeight: target.name !== 'itemHeight' ? previousState.itemDetails.itemHeight : parseFloat(target.value) || '',
+                    itemColor: target.name !== 'itemColor' ? previousState.itemDetails.itemColor : target.value,
+                    itemQuantity: target.name !== 'itemQuantity' ? previousState.itemDetails.itemQuantity : parseFloat(target.value) || '',
+                    itemId: previousState.itemDetails.itemId
+                }
+            }
+        });
+        // this.props.onBinPropsUpdate({ target });
+    }
+
     render() {
         return(
             <React.Fragment>
-                <h5>Item 4</h5>
+                <h5>Item {this.state.index + 1}</h5>
                 <div className="row">
                     <div className="col-lg-4 container-spotter-input-label-block">
                         <label htmlFor="item4XCoordinate">Length</label>
-                        <input type="number" className="form-control" name="item4XCoordinate" id="item4XCoordinate" placeholder="4" />
+                        <input 
+                            type="number" 
+                            className="form-control" 
+                            name='itemLength'
+                            placeholder="4" 
+                            value={this.state.itemDetails.itemLength}
+                            onChange={this.handleChange}
+                            onKeyUp={this.handleChange}
+                        />
                     </div>
                     <div className="col-lg-4 container-spotter-input-label-block">
-                        <label htmlFor="item4YCoordinate">Width</label>
-                        <input type="number" className="form-control" name="item4YCoordinate" id="item4YCoordinate" placeholder="5" />
+                        <label htmlFor="itemWidth">Width</label>
+                        <input 
+                            type="number" 
+                            className="form-control" 
+                            name="itemWidth"
+                            placeholder="5" 
+                            value={this.state.itemDetails.itemWidth}
+                            onChange={this.handleChange}
+                            onKeyUp={this.handleChange}
+                        />
                     </div>
                     <div className="col-lg-4 container-spotter-input-label-block">
-                        <label htmlFor="item4ZCoordinate">Height</label>
-                        <input type="number" className="form-control" name="item4ZCoordinate" id="item4ZCoordinate" placeholder="3" />
+                        <label htmlFor="itemHeight">Height</label>
+                        <input 
+                            type="number" 
+                            className="form-control" 
+                            name="itemHeight"
+                            placeholder="6" 
+                            value={this.state.itemDetails.itemHeight}
+                            onChange={this.handleChange}
+                            onKeyUp={this.handleChange}
+                        />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-lg-6 container-spotter-input-label-block">
-                        <label htmlFor="item4Quantity">Quantity</label>
-                        <input type="number" className="form-control" name="item4Quantity" id="item4Quantity" placeholder="1" />
+                        <label htmlFor="itemQuantity">Quantity</label>
+                        <input 
+                            type="number" 
+                            className="form-control" 
+                            name="itemQuantity"
+                            placeholder="1" 
+                            value={this.state.itemDetails.itemQuantity}
+                            onChange={this.handleChange}
+                            onKeyUp={this.handleChange}
+                        />
                     </div>
                     <div className="col-lg-6 container-spotter-input-label-block">
-                        <label htmlFor="item4Color">Box Color</label>
-                        <input type="text" className="form-control" name="item4Color" id="item4Color" placeholder="0xffff00" />
+                        <label htmlFor="itemColor">Box Color</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            name="itemColor"
+                            placeholder="0xffff00" 
+                            value={this.state.itemDetails.itemColor}
+                            onChange={this.handleChange}
+                            onKeyUp={this.handleChange}
+                        />
                     </div>
                 </div>
                 <hr className="container-spotter-item-divider-hr"></hr>
